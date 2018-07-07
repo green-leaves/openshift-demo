75eea3490df26738f651167add122b3b4579bc11
Nguyen Duy Tiep <duytiep@gmail.com>
2018-07-07 23:03:54 +0800

    add jaeger tracing

--- hello-service/src/main/java/com/greenleaves/openshift/HelloApplication.java
+++ hello-service/src/main/java/com/greenleaves/openshift/HelloApplication.java
@@ -7,15 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class HelloApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HelloApplication.class, args);
@@ -27,8 +24,8 @@ public class HelloApplication {
     }
 }
 
-@RestController
 @Slf4j
+@RestController
 class HelloController {
     @Value("${greeting.message}")
     private String message;
@@ -36,14 +33,10 @@ class HelloController {
 	@Value("${greeting.test}")
 	private String test;
 
-	final
-    private DiscoveryClient discoveryClient;
-
 	private final RestTemplate restTemplate;
 
     @Autowired
-    public HelloController(DiscoveryClient discoveryClient, RestTemplate restTemplate) {
-        this.discoveryClient = discoveryClient;
+    public HelloController(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
 
@@ -51,7 +44,6 @@ class HelloController {
 	public String get() {
         log.info(test);
         String ip = restTemplate.getForObject("http://ip-service:8080/", String.class);
-
 		return "Hello, " + message + ", you are: " + ip;
 	}
 }
--- ip-service/src/main/java/com/greenleaves/openshift/ipservice/IpApplication.java
+++ ip-service/src/main/java/com/greenleaves/openshift/ipservice/IpApplication.java
@@ -4,8 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +13,6 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class IpApplication {
 
 	public static void main(String[] args) {
@@ -33,22 +30,19 @@ public class IpApplication {
 class IpController {
 
 	private final
-	DiscoveryClient discoveryClient;
-
-	private final
 	RestTemplate restTemplate;
 
+
 	@Autowired
-	public IpController(DiscoveryClient discoveryClient, RestTemplate restTemplate) {
-		this.discoveryClient = discoveryClient;
+	public IpController(RestTemplate restTemplate) {
 		this.restTemplate = restTemplate;
 	}
 
 	@GetMapping
 	public String get() throws UnknownHostException {
 		InetAddress inetAddress = InetAddress.getLocalHost();
-		String greeting = restTemplate.getForObject("http://hello-service:8080/", String.class);
-		log.info(greeting);
+		//String greeting = restTemplate.getForObject("http://hello-service:8080/", String.class);
+		//log.info(greeting);
 		log.info(inetAddress.getHostName());
 		log.info(inetAddress.getHostAddress());
 		return System.getenv("HOSTNAME") + ", " + inetAddress.getHostName() + ", " + inetAddress.getHostAddress();
