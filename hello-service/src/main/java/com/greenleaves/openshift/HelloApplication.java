480cb82e9260375efebabbe0fa33fa879f2cf7ee
Nguyen Duy Tiep <duytiep@gmail.com>
2018-08-26 20:32:09 +0800

    Kubernetes reloadable config

--- hello-service/src/main/java/com/greenleaves/openshift/HelloApplication.java
+++ hello-service/src/main/java/com/greenleaves/openshift/HelloApplication.java
@@ -1,18 +1,21 @@
 package com.greenleaves.openshift;
 
+import lombok.Getter;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableConfigurationProperties({GreetingProperties.class})
 public class HelloApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HelloApplication.class, args);
@@ -27,23 +30,33 @@ public class HelloApplication {
 @Slf4j
 @RestController
 class HelloController {
-    @Value("${greeting.message}")
-    private String message;
 
-	@Value("${greeting.test}")
-	private String test;
+    final
+    GreetingProperties greeting;
 
 	private final RestTemplate restTemplate;
 
     @Autowired
-    public HelloController(RestTemplate restTemplate) {
+    public HelloController(RestTemplate restTemplate, GreetingProperties greetingProperties) {
         this.restTemplate = restTemplate;
+        this.greeting = greetingProperties;
     }
 
     @GetMapping("/")
 	public String get() {
-        log.info(test);
-        String ip = restTemplate.getForObject("http://ip-service:8080/", String.class);
-		return "Hello, " + message + ", you are: " + ip;
+        log.info(greeting.getTest());
+        log.info(greeting.getInfo());
+        //String ip = restTemplate.getForObject("http://ip-service:8080/", String.class);
+		//return "Hello, " + message + ", you are: " + ip;
+		return "Hello, " + greeting.getMessage();
 	}
+}
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "greeting")
+class GreetingProperties {
+    private String message;
+    private String test;
+    private String info;
 }
