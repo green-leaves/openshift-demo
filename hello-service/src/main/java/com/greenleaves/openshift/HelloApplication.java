f9a726e6208154031b2f1676d8cedba34a3c9624
Nguyen Duy Tiep <duytiep@gmail.com>
2018-12-16 14:20:24 +0800

    Kubernetes camel leader election

--- hello-service/src/main/java/com/greenleaves/openshift/HelloApplication.java
+++ hello-service/src/main/java/com/greenleaves/openshift/HelloApplication.java
@@ -3,6 +3,8 @@ package com.greenleaves.openshift;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.camel.cluster.CamelClusterEventListener;
+import org.apache.camel.cluster.CamelClusterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,10 +12,17 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import javax.annotation.PostConstruct;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@Slf4j
 @SpringBootApplication
 @EnableConfigurationProperties({GreetingProperties.class})
 public class HelloApplication {
@@ -21,9 +30,40 @@ public class HelloApplication {
 		SpringApplication.run(HelloApplication.class, args);
 	}
 
+	@Autowired
+    ElectionService electionService;
+
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Scheduled(fixedRate = 1000L)
+    public void leaderWrite() throws UnknownHostException {
+	    if (electionService.isLeader()) {
+            log.info("I am leader {}", InetAddress.getLocalHost().getHostName());
+        }
+    }
+
+
+
+}
+
+@Slf4j
+@Component
+@Getter
+class ElectionService {
+    private boolean isLeader;
+
+    @Autowired
+    CamelClusterService clusterService;
+
+    @PostConstruct
+    public void init() throws Exception {
+        clusterService.getView("lock2").addEventListener((CamelClusterEventListener.Leadership) (view, leader) -> {
+            // here we get a notification of a change in the leadership
+            this.isLeader = leader.isPresent() && leader.get().isLocal();
+        });
     }
 }
 
